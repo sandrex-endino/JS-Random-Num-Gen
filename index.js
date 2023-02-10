e6e4edcbd0688113b08fa85b5bd9e98ab9a6f231
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-02-10 18:27:12 +0800

    Added toggle dark/light mode, css style, js script

--- index.js
+++ index.js
@@ -2,17 +2,38 @@ window.onload = function() {
 
 // let displayRandomNum = 
 let buttonGenerateRandNum = document.getElementById('buttonClick');
-let buttonReset = document.getElementById('buttonReset');
+let clearButton = document.getElementById('buttonReset');
+let modeButton = document.getElementById('darkMode');
 
 buttonGenerateRandNum.onclick = function() {randomNum(1, 100)};
 
     function randomNum(min, max) {       
         document.getElementById('randomNum').innerHTML = (Math.random() * (max - min) + min).toFixed(0);
     }
-};
 
-buttonReset.onclick = function() {clearP()};
+
+clearButton.onclick = function() {clearP()};
 
     function clearP() {
         document.getElementById('randomNum').innerHTML = '';
     }
+
+
+modeButton.onclick = function() {darkMode()};
+
+    function darkMode() {
+        let elementBody = document.body;
+        elementBody.classList.toggle("dark-mode");
+
+        let elementButtonClick = document.getElementById('buttonClick');
+        elementButtonClick.classList.toggle("button-dark-mode-click");
+
+        let elementButtonReset = document.getElementById('buttonReset');
+        elementButtonReset.classList.toggle("button-dark-mode-reset");
+
+        let elementButtonDarkMode = document.getElementById('darkMode');
+        elementButtonDarkMode.classList.toggle("button-dark-mode-dark");
+    }
+
+}
+
