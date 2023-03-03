232bae4ff7482f715e4ea0c520b8983b604ba892
Sandrex Endino <115247135+5andrex@users.noreply.github.com>
2023-03-03 10:12:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-window.onload = function() {
+// For random number generation
 
-// let displayRandomNum = 
-let buttonGenerateRandNum = document.getElementById('buttonClick');
-let clearButton = document.getElementById('buttonReset');
-let modeButton = document.getElementById('darkMode');
+// button that generates random number when clicked
+  const randomBtn = document.getElementById('random-btn');
 
-buttonGenerateRandNum.onclick = function() {randomNum(1, 100)};
+  randomBtn.onclick = () => {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    document.getElementById('random-p').innerHTML = randomNum;
+  }
 
-    function randomNum(min, max) {       
-        document.getElementById('randomNum').innerHTML = (Math.random() * (max - min) + min).toFixed(0);
-    }
+// button that clears the random number div
+  const clearBtn = document.getElementById('clear-btn');
+
+  clearBtn.onclick = () => {
+    document.getElementById('random-p').innerHTML = " ";
+  }
+
+  
+// For theme selector
+
+// changes the html and body theme and btn theme
+const select = document.getElementById('switch-theme');
+const html = document.querySelector('html');
+document.body.style.padding = '10px';
+
+let isDarkMode = false;
 
 
-clearButton.onclick = function() {clearP()};
+function updateTheme() {
+  const bgColor = isDarkMode ? 'black' : 'white';
+  const textColor = isDarkMode ? 'white' : 'black';
 
-    function clearP() {
-        document.getElementById('randomNum').innerHTML = '';
-    }
-
-
-modeButton.onclick = function() {darkMode()};
-
-    function darkMode() {
-        let elementBody = document.body;
-        elementBody.classList.toggle("dark-mode");
-
-        let elementButtonClick = document.getElementById('buttonClick');
-        elementButtonClick.classList.toggle("button-dark-mode-click");
-
-        let elementButtonReset = document.getElementById('buttonReset');
-        elementButtonReset.classList.toggle("button-dark-mode-reset");
-
-        let elementButtonDarkMode = document.getElementById('darkMode');
-        elementButtonDarkMode.classList.toggle("button-dark-mode-dark");
-    }
-
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+  randomBtn.style.backgroundColor = bgColor;
+  randomBtn.style.color = textColor;
+  clearBtn.style.backgroundColor = bgColor;
+  clearBtn.style.color = textColor;
 }
 
+select.addEventListener('change', () => {
+  isDarkMode = !isDarkMode;
+  if (select.checked) {
+    updateTheme();
+  } else {
+    updateTheme();
+  }
+});
